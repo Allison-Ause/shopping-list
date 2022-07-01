c5f36a5dc698adcd50a5552a23c3bc7c3bc0f04b
Allison Ause <allison.ause@gmail.com>
2022-07-01 09:42:07 -0700

    eliminated console statement

--- components/DeleteList.js
+++ components/DeleteList.js
@@ -1,6 +1,6 @@
 
 export default function createDeleteList(itemList, { handleDeleteAllItems }) {
-    console.log('firing component');
+
     const deleteButton = document.querySelector('#delete-button');
     deleteButton.addEventListener('click', () => {
         handleDeleteAllItems(itemList);
